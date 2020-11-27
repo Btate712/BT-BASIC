5406b37c3c7f64b8dad3a025c3e354e1b1f23520
btate712 <btate712@gmail.com>
2020-11-27 17:30:53 -0600

    tested for floating values

--- test/Scanner.test.js
+++ test/Scanner.test.js
@@ -3,11 +3,18 @@ const Scanner = require("../src/Scanner.js");
 test("Should be able to instantiate a Scanner", () => {
   let scanner = new Scanner("");
   expect(scanner.getTokens()).toEqual([]);
-})
+});
 
-test("Can identify a numeric token", () => {
+test("Can identify an integer token", () => {
   let scanner = new Scanner("805");
   scanner.tokenize();
   expect(scanner.getTokens()[0].getType()).toEqual("NUMBER");
   expect(scanner.getTokens()[0].toString()).toEqual("NUMBER: 805");
-})
+});
+
+test("Can identify a numeric floating value token", () => {
+  let scanner = new Scanner("11.94");
+  scanner.tokenize();
+  expect(scanner.getTokens()[0].getType()).toEqual("NUMBER");
+  expect(scanner.getTokens()[0].toString()).toEqual("NUMBER: 11.94");
+});
